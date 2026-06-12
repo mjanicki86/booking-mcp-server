9aad51b1edf9ca341ee76deb79ffb0638e2e3232
mjanicki86 <m.janicki@wecotravel.pl>
2026-06-12 13:03:39 +0200

    Delete dist directory

--- dist/tools/hotelSearch.d.ts
+++
@@ -1,4 +0,0 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { BookingApiClient } from "../services/bookingClient.js";
-export declare function registerHotelSearchTool(server: McpServer, client: BookingApiClient, apiKey: string): void;
-//# sourceMappingURL=hotelSearch.d.ts.map
--- dist/tools/listCities.d.ts
+++
@@ -1,3 +0,0 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-export declare function registerSearchCitiesTool(server: McpServer, apiKey: string): void;
-//# sourceMappingURL=listCities.d.ts.map
